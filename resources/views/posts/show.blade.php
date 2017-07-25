8bd819dc3ce9b181575072e57ce6dc03b73b84f3
Sammilynn <hollow71@students.rowan.edu>
2017-07-24 21:47:49 -0400

    bootstrapped

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>A Place to show the post</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@ use App\Task;
 |
 */
 
-Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
+Route::get('/', 'PostsController@index');
+
+Route::get('/posts/create', 'PostsController@create');
+
+Route::get('/posts/{post}', 'PostsController@show');
+
+Route::post('/posts', 'PostsController@store');
